061eeebe5e2804c920d6bc16533c79ba18cf52f3
daydreamer <nimandrey22@gmail.com>
2025-04-17 18:44:27 +0300

    Сдача проектной работы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,76 @@
 import { ArrowButton } from 'src/ui/arrow-button';
-import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+import { ReactNode, useEffect, useRef } from 'react';
 
-export const ArticleParamsForm = () => {
+export type TArticleParamsFormChildren = {
+	formTitle: ReactNode;
+	fontSelector: ReactNode;
+	fontSizeSelector: ReactNode;
+	fontColorSelector: ReactNode;
+	separator: ReactNode;
+	backgroundColor: ReactNode;
+	widthContent: ReactNode;
+	resetButton: ReactNode;
+	confirmButton: ReactNode;
+};
+
+export type TArticleParamsForm = {
+	isOpen: boolean;
+	onClick: () => void;
+	children: TArticleParamsFormChildren;
+};
+
+export const ArticleParamsForm = (props: TArticleParamsForm) => {
+	const { isOpen, onClick } = props;
+	const {
+		formTitle,
+		fontSelector,
+		fontSizeSelector,
+		fontColorSelector,
+		separator,
+		backgroundColor,
+		widthContent,
+		resetButton,
+		confirmButton,
+	} = props.children;
+	const asideRef = useRef<HTMLElement>(null);
+
+	useEffect(() => {
+		if (!isOpen) return;
+
+		const handleClickOutside = (event: MouseEvent) => {
+			const { target } = event;
+			if (target instanceof Node && !asideRef.current?.contains(target)) {
+				onClick();
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen, onClick]);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={onClick} />
+			<aside
+				ref={asideRef}
+				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					{formTitle}
+					{fontSelector}
+					{fontSizeSelector}
+					{fontColorSelector}
+					{separator}
+					{backgroundColor}
+					{widthContent}
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						{resetButton}
+						{confirmButton}
 					</div>
 				</form>
 			</aside>
